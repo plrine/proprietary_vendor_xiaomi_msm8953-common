e0817d6a113cc8def455c9139a43208e3239f66b
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 09:09:32 +0200

    msm8953-common: Remove OMX audio codecs
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
     * OMX SW audio codecs no longer exist.
    
    Change-Id: I940be6716b603e848c44839bb87ad690eb55e86f

--- msm8953-common-vendor.mk
+++ msm8953-common-vendor.mk
@@ -107,14 +107,6 @@ PRODUCT_PACKAGES += \
     vulkan.msm8953 \
     libC2D2 \
     libCB \
-    libOmxAacDec \
-    libOmxAlacDec \
-    libOmxAmrDec \
-    libOmxAmrwbplusDec \
-    libOmxApeDec \
-    libOmxEvrcDec \
-    libOmxQcelp13Dec \
-    libOmxWmaDec \
     libOpenCL \
     libQSEEComAPI \
     libacdb-fts \
@@ -155,7 +147,6 @@ PRODUCT_PACKAGES += \
     libmdmdetect \
     libmdsprpc \
     libminksocket \
-    libmm-color-convertor \
     libmm-hdcpmgr \
     libperipheral_client \
     libqcci_legacy \
@@ -201,7 +192,6 @@ PRODUCT_PACKAGES += \
     libadm \
     libadpcmdec \
     libdrc \
-    libmmosal_proprietary \
     libsmwrapper \
     libsurround_3mic_proc \
     libvqzip \
